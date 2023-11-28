adc87d3f890de04a442d6b6bfb031905b80ed2d5
lgndev <logan.sawyer.dev@gmail.com>
2023-11-28 14:56:19 -0500

    emotion css prop is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { css } from "@emotion/react";
 
 const App = () => {
   return (
     <>
-      <div>form</div>
+      <div
+        css={css`
+          padding: 32px;
+          background-color: hotpink;
+          font-size: 24px;
+          border-radius: 4px;
+        `}
+      >
+        form
+      </div>
     </>
   );
 };
