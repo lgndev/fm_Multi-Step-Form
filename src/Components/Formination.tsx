1771e5724172caf802003df3bbeb0375463658aa
lgndev <logan.sawyer.dev@gmail.com>
2023-12-04 12:53:25 -0500

    Formination mobile/desktop styles complete

--- src/Components/Formination.tsx
+++ src/Components/Formination.tsx
@@ -1,17 +1,36 @@
 import { css } from "@emotion/react";
+import { breakpoints } from "../emotionStyles/styles";
 
-// const sideBarContainerCss = css({
-//   height: "172px",
-//   gridColumn: "1 / 2",
-//   gridRow: "1 / 2",
-//   backgroundImage: `url(${sideBarMobile})`,
-//   backgroundSize: "cover",
-//   backgroundRepeat: "no-repeat",
-//   backgroundPosition: "center",
-// });
+const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
+
+const forminationCss = css({
+  width: "33px",
+  height: "33px",
+  borderRadius: "50%",
+  border: "1px solid #BEE2FD",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontFamily: "'Ubuntu - bold'",
+  fontSize: "14px",
+  color: "#BEE2FD",
+  margin: "16px",
+  [mq[0]]: {
+    margin: "16px 16px 16px 0",
+  },
+});
 
 const Formination = () => {
-  return <div css={""}>Formination</div>;
+  return (
+    <div
+      css={css([
+        forminationCss,
+        true ? { backgroundColor: "#BEE2FD", color: "#022959" } : "",
+      ])}
+    >
+      1
+    </div>
+  );
 };
 
 export default Formination;
--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -1,6 +1,7 @@
 import { css } from "@emotion/react";
 import sideBarMobile from "../assets/images/bg-sidebar-mobile.svg";
 import { breakpoints } from "../emotionStyles/styles";
+import Formination from "./Formination";
 
 const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
 
@@ -13,6 +14,9 @@ const sideBarContainerCss = css({
   backgroundRepeat: "no-repeat",
   backgroundPosition: "center",
   padding: "32px 16px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "flex-start",
   [mq[0]]: {
     width: "274px",
     height: "auto",
@@ -20,11 +24,20 @@ const sideBarContainerCss = css({
     gridRow: "1 / 3",
     borderRadius: "10px",
     padding: "40px 32px",
+    flexDirection: "column",
+    justifyContent: "flex-start",
   },
 });
 
 const SideBar = () => {
-  return <div css={sideBarContainerCss}>SideBar</div>;
+  return (
+    <div css={sideBarContainerCss}>
+      <Formination />
+      <Formination />
+      <Formination />
+      <Formination />
+    </div>
+  );
 };
 
 export default SideBar;
